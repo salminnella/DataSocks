ab4edff609b5c267c645f1c8d4e8c55725fda6c3
salminnella <salminnella@gmail.com>
2016-11-01 02:07:51 -0700

    template for python client socket to android device comm set

--- AnSockets/app/src/main/java/com/example/salminnella/ansockets/MainActivity.java
+++ AnSockets/app/src/main/java/com/example/salminnella/ansockets/MainActivity.java
@@ -1,15 +1,39 @@
 package com.example.salminnella.ansockets;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    // DEFAULT IP
+    public static String SERVERIP = "10.0.2.15";
+
+    // DESIGNATE A PORT
+    public static final int SERVERPORT = 9999;
+
+    private TextView mainTextView;
+    private TextView serverStatus;
+    private TextView thisIpTextView;
+    private FloatingActionButton fab;
+    private Handler handler = new Handler();
+    private ServerSocket serverSocket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +42,24 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        initViews();
+        initFab();
+
+        SERVERIP = getLocalIpAddress(true);
+
+        Thread fst = new Thread(new ServerThread());
+        fst.start();
+
+    }
+
+    private void initViews() {
+        mainTextView = (TextView) findViewById(R.id.hello_text_view);
+        serverStatus = (TextView) findViewById(R.id.server_status_text_view);
+        thisIpTextView = (TextView) findViewById(R.id.this_ip_text_view);
+    }
+
+    private void initFab() {
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,25 +69,140 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public class ServerThread implements Runnable {
+
+        public void run() {
+            try {
+                if (SERVERIP != null) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            serverStatus.setText("Listening on IP: " + SERVERIP);
+
+                        }
+                    });
+                    serverSocket = new ServerSocket(SERVERPORT);
+                    while (true) {
+                        // LISTEN FOR INCOMING CLIENTS
+                        Socket client = serverSocket.accept();
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                serverStatus.setText("Connected.");
+                            }
+                        });
+
+                        try {
+                            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                            String line = null;
+                            while ((line = in.readLine()) != null) {
+                                Log.d("ServerActivity", line);
+                                final String finalLine = line;
+                                handler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        mainTextView.setText(finalLine);
+                                    }
+                                });
+                            }
+                            break;
+                        } catch (Exception e) {
+                            handler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    serverStatus.setText("Oops. Connection interrupted.");
+                                }
+                            });
+                            e.printStackTrace();
+                        }
+                    }
+                } else {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            serverStatus.setText("Couldn't detect internet connection.");
+                        }
+                    });
+                }
+            } catch (Exception e) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        serverStatus.setText("Error");
+                    }
+                });
+                e.printStackTrace();
+            }
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+
+
+
+
+
+    // GETS THE IP ADDRESS OF YOUR PHONE'S NETWORK
+    private String getLocalIpAddress(boolean useIPv4) {
+//        try {
+//            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+//                NetworkInterface intf = en.nextElement();
+//                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+//                    InetAddress inetAddress = enumIpAddr.nextElement();
+////                    if (!inetAddress.isLoopbackAddress()) { return inetAddress.getCanonicalHostName().toString(); }
+//                    if (!inetAddress.isLoopbackAddress() && InetAddressUtils.isIPv4Address(ipv4 = inetAddress.getHostAddress())) {
+//
+//                        String ip = inetAddress.getHostAddress().toString();
+//                        System.out.println("ip---::" + ip);
+//                        EditText tv = (EditText) findViewById(R.id.ipadd);
+//                        tv.setText(ip);
+//                        // return inetAddress.getHostAddress().toString();
+//                        return ip;
+//                    }
+//                }
+//            }
+//        } catch (SocketException ex) {
+//            Log.e("ServerActivity", ex.toString());
+//        }
+//        return null;
+
+
+        try {
+            List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface intf : interfaces) {
+                List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
+                for (InetAddress addr : addrs) {
+                    if (!addr.isLoopbackAddress()) {
+                        String sAddr = addr.getHostAddress();
+                        //boolean isIPv4 = InetAddressUtils.isIPv4Address(sAddr);
+                        boolean isIPv4 = sAddr.indexOf(':')<0;
+
+                        if (useIPv4) {
+                            if (isIPv4)
+                                return sAddr;
+                        } else {
+                            if (!isIPv4) {
+                                int delim = sAddr.indexOf('%'); // drop ip6 zone suffix
+                                return delim<0 ? sAddr.toUpperCase() : sAddr.substring(0, delim).toUpperCase();
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception ex) { } // for now eat exceptions
+        return "";
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        try {
+            // MAKE SURE YOU CLOSE THE SOCKET UPON EXITING
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
